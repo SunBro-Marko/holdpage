e1cd96bc7307ceb71e17bb55add0864e9c44b163
Михаил Алёнин <m.alenin@awatera.com>
2021-04-11 21:12:39 +0300

    Обновление favicon

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Технические работы</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <AbstractComponent type={pageType} variants={headers} />
       <AbstractComponent type={pageType} variants={mains} />
